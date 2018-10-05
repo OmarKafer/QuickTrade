7cb6a4e9dcd551dbdd034442b48275f1aca8314c
Omar Kafernawi <omarku5@hotmail.com>
2018-10-05 12:59:15 +0200

    Terminado, falta revisar RegistroActivity La App se cierra al lanzar esa vista.

--- app/src/main/java/omar/pmdm/quicktrade/LoginActivity.java
+++ app/src/main/java/omar/pmdm/quicktrade/LoginActivity.java
@@ -1,11 +1,12 @@
 package omar.pmdm.quicktrade;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -20,11 +21,30 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Inicializamos los botones y campos de texto que vamos a necesitar de la vista
+        final Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        final Button btnRegistrar = (Button) findViewById(R.id.btnRegistrar);
         usuario = (EditText)findViewById(R.id.txtUsuario);
         password = (EditText)findViewById(R.id.txtPassword);
         listaUsuarios = new ArrayList<Usuario>();
-        guardarUsuario(1, "Manel", "Viel", "mviel@florida-uni.es", "1234", "654321098");
 
+        // Listener del botón "btnLogin"
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                comprobarUsuario(v);
+            }
+        });
+
+        // Listener del botón "btnRegistrar"
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), RegistroActivity.class);
+                i.putExtra("listaUsuarios", listaUsuarios);
+                startActivityForResult(i, 0);
+            }
+        });
+
+        guardarUsuario(1, "Manel", "Viel", "mviel@florida-uni.es", "1234", "654321098");
     }
 
     private void guardarUsuario(int idUser, String nombre, String apellidos, String email, String password, String telefono) {
@@ -32,25 +52,24 @@ public class LoginActivity extends AppCompatActivity {
         listaUsuarios.add(u1);
     }
 
-    public void comprobarUsuario(View v) {
+    private void comprobarUsuario(View v) {
         // Comprobamos que ninguno de los campos sea una cadena vacia, en caso de serlo devolvemos false;
-        if((usuario.getText().toString().compareTo("") != 0) || (password.getText().toString().compareTo("") != 0)) {
-            Log.d("Depuración", "Una de las cadenas está vacía");
-        }
-        Usuario u1;
-        Iterator<Usuario> Iter = listaUsuarios.iterator();
-        while(Iter.hasNext()) {
-            u1 = (Usuario) Iter.next();
-            Log.d("Depuración", "Usuario introducido: " + usuario.getText());
-            Log.d("Depuración", "Usuario a comparar: " + u1.getEmail());
-            Log.d("Depuración", "Contraseña introducida: " + password.getText());
-            Log.d("Depuración", "Contraseña a comparar: " + u1.getPassword());
-            if(usuario.getText().toString().compareTo(u1.getEmail()) == 0) {
-                if(password.getText().toString().compareTo(u1.getPassword()) == 0) {
-                    Log.d("Depuración", "El usuario y la clave son correctas");
+        if((usuario.getText().toString().compareTo("") == 0) || (password.getText().toString().compareTo("") == 0)) {
+            // Alguna cadena vacía
+        } else {
+            Usuario u1;
+            Iterator<Usuario> Iter = listaUsuarios.iterator();
+            while (Iter.hasNext()) {
+                u1 = (Usuario) Iter.next();
+                if (usuario.getText().toString().compareTo(u1.getEmail()) == 0) {
+                    if (password.getText().toString().compareTo(u1.getPassword()) == 0) {
+                        // Coincide el usuario y la clave con el que estamos comparando ahora mismo;
+                        Intent i = new Intent(v.getContext(), MainActivity.class);
+                        startActivity(i);
+                    }
                 }
             }
+            // No ha coincidido ningun usuario (el usuario no existe o la clave es incorrecta);
         }
-        Log.d("Depuración", "No existe el usuario o la contraseña es incorrecta");
     }
 }
--- app/src/main/java/omar/pmdm/quicktrade/RegistroActivity.java
+++ app/src/main/java/omar/pmdm/quicktrade/RegistroActivity.java
@@ -1,7 +1,14 @@
 package omar.pmdm.quicktrade;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import java.util.ArrayList;
 
 public class RegistroActivity extends AppCompatActivity {
 
@@ -9,5 +16,63 @@ public class RegistroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        final Button btnCancelar = (Button) findViewById(R.id.btnCancelar);
+        final Button btnGuardar = (Button) findViewById(R.id.btnGuardar);
+        Bundle bundle = getIntent().getExtras();
+        final ArrayList<Usuario> listaUsuarios = (ArrayList<Usuario>) bundle.getSerializable("listaUsuarios");
+
+
+        // Listener del botón "btnCancelar"
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), LoginActivity.class);
+                startActivityForResult(i, 0);
+            }
+        });
+
+        // Listener del botón "btnGuardar"
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                guardarUsuario(listaUsuarios);
+                Intent i = new Intent(v.getContext(), LoginActivity.class);
+                startActivityForResult(i, 0);
+            }
+        });
+    }
+
+    private void guardarUsuario(ArrayList<Usuario> listaUsuarios) {
+        if(comprobarCampos()) {
+            EditText idUsuario = (EditText) findViewById(R.id.txtId);
+            EditText nombre = (EditText) findViewById(R.id.txtId);
+            EditText apellidos = (EditText) findViewById(R.id.txtId);
+            EditText email = (EditText) findViewById(R.id.txtId);
+            EditText password = (EditText) findViewById(R.id.txtId);
+            EditText telefono = (EditText) findViewById(R.id.txtId);
+            Usuario u1 = new Usuario(Integer.parseInt(idUsuario.getText().toString()), nombre.getText().toString(), apellidos.getText().toString(), email.getText().toString(), password.getText().toString(), telefono.getText().toString());
+            listaUsuarios.add(u1);
+        }
+    }
+
+    private boolean comprobarCampos() {
+        EditText idUsuario = (EditText) findViewById(R.id.txtId);
+        EditText nombre = (EditText) findViewById(R.id.txtId);
+        EditText apellidos = (EditText) findViewById(R.id.txtId);
+        EditText email = (EditText) findViewById(R.id.txtId);
+        EditText password = (EditText) findViewById(R.id.txtId);
+        EditText telefono = (EditText) findViewById(R.id.txtId);
+        RadioButton hombre = (RadioButton) findViewById(R.id.rbHombre);
+        RadioButton mujer = (RadioButton) findViewById(R.id.rbMujer);
+        if(idUsuario.getText().toString().compareTo("") == 0 ||
+                nombre.getText().toString().compareTo("") == 0 ||
+                apellidos.getText().toString().compareTo("") == 0 ||
+                email.getText().toString().compareTo("") == 0 ||
+                password.getText().toString().compareTo("") == 0 ||
+                telefono.getText().toString().compareTo("") == 0 ||
+                (!hombre.isChecked() && !mujer.isChecked())) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
